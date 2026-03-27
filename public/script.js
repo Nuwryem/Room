6d200132676599b7c41722a8efc0939a7e006769
Nuwryem <nuwryem@gmail.com>
2026-03-27 22:48:10 +0530

    add timestamps UI

--- public/script.js
+++ public/script.js
@@ -39,16 +39,29 @@ function sendMessage() {
 function addMessage(msg) {
     const div = document.createElement('div');
 
+    let time = document.createElement('span');
+    time.style.fontSize = "10px";
+    time.style.opacity = "0.6";
+    time.innerText = " " + msg.time;
+
     if (msg.user === "System") {
         div.className = "message system";
         div.innerText = msg.text;
     } else if (msg.user === username) {
         div.className = "message me";
         div.innerText = msg.text;
+        div.appendChild(time);
     } else {
         div.className = "message other";
         div.innerText = msg.user + ": " + msg.text;
+        div.appendChild(time);
     }
+
+    document.getElementById('messages').appendChild(div);
+
+    const msgBox = document.getElementById('messages');
+    msgBox.scrollTop = msgBox.scrollHeight;
+}
 
     document.getElementById('messages').appendChild(div);
 
